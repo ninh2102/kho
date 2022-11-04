d2a9002e8c8e56abc413d146f09da834dadcb5af
thuong <dh51902991@student.stu.edu.vn>
2022-11-04 14:39:08 +0700

    hello

--- Source.cpp
+++ Source.cpp
@@ -5,17 +5,7 @@
 //hehe
 >>>>>>> eaa00e41f1531f3af449623e7489fdb0197265c2
 using namespace std;
-int cong(int a, int b);//SV A
-int tru(int a, int b);//SV B
-int nhan(int a, int b);//SV C
-int chia(int a, int b);//SV D
-int main()
-{
-	int a, b;
-	cout << "CHUONG TRINH TINH TOAN CO BAN" << endl;
-	//Cac ban code tai day
-
-	cout << endl;
-	system("pause");
+int main(){
+	cout<<"hello";
 	return 0;
 }
