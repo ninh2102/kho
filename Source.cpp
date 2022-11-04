8f7dfe57e7365076d82f25e4b3e6a46ad8257716
ninh <dh51905239@student.stu.edu.vn>
2022-11-04 14:35:37 +0700

    ptbac1

--- Source.cpp
+++ Source.cpp
@@ -5,12 +5,12 @@
 //hehe
 >>>>>>> eaa00e41f1531f3af449623e7489fdb0197265c2
 using namespace std;
-int cong(int a, int b);//SV A
+/*int cong(int a, int b);//SV A
 int tru(int a, int b);//SV B
 int nhan(int a, int b);//SV C
-int chia(int a, int b);//SV D
+int chia(int a, int b);//SV D*/
 int main()
-{
+/*{
 	int a, b;
 	cout << "CHUONG TRINH TINH TOAN CO BAN" << endl;
 	//Cac ban code tai day
@@ -18,4 +18,24 @@ int main()
 	cout << endl;
 	system("pause");
 	return 0;
+}*/
+{
+    int a;
+    int b;
+    double nghiem;
+    cout << "Nhap a = ";
+    cin >> a;
+    cout << "Nhap b = ";
+    cin >> b;
+    if(a == 0) {
+        if (b == 0) {
+            cout << "Phuong trinh vo so nghiem" << endl;
+        } else {
+            cout << "Phuong trinh vo nghiem" << endl;
+        }
+    } else {
+        nghiem = (double)-b/a;
+        cout << "Phuong trinh co nghiem la: " << nghiem << endl;
+    }
+    return 0;
 }
